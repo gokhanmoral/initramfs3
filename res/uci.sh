6414dd59b9c56e28e5163ffa8b89021569abdff7
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-01 17:39:05 +0300

    fixed select profile and prevent writing back current settings on profile operations

--- res/uci.sh
+++ res/uci.sh
@@ -17,12 +17,15 @@ read_config
 case "${1}" in
   rename)
     rename_profile "$2" "$3"
+    exit 0
     ;;
   delete)
     delete_profile "$2"
+    exit 0
     ;;
   select)
     select_profile "$2"
+    exit 0
     ;;
   config)
     print_config
