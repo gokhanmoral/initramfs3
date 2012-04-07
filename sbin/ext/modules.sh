6f8897ac56fea52e581410acbd45643dcba4a082
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-07 08:10:46 +0300

    workaround for module loading problem and lowered vitalij's value to 3

--- sbin/ext/modules.sh
+++ sbin/ext/modules.sh
@@ -14,10 +14,19 @@ read_config
 if [ "$logger" == "off" ];then
 rm -rf /dev/log
 fi
+
 if [ "$logger" == "on" ];then
-insmod /lib/modules/logger.ko
+/sbin/insmod /lib/modules/logger.ko
+/sbin/insmod /lib/modules/logger.ko
+/sbin/insmod /lib/modules/logger.ko
 fi
+
 #fm radio, I have no idea why it isn't loaded in init -gm
-insmod /lib/modules/Si4709_driver.ko
+/sbin/insmod /lib/modules/Si4709_driver.ko
+/sbin/insmod /lib/modules/Si4709_driver.ko
+/sbin/insmod /lib/modules/Si4709_driver.ko
+
 # for ntfs automounting
-insmod /lib/modules/fuse.ko
+/sbin/insmod /lib/modules/fuse.ko
+/sbin/insmod /lib/modules/fuse.ko
+/sbin/insmod /lib/modules/fuse.ko
