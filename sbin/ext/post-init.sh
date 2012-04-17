84271997fe8ddc8795f7b1181827cd541aaf04ba
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-17 16:03:12 +0300

    some refactoring in scripts

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -5,7 +5,38 @@
 #exec >>/data/user.log
 #exec 2>&1
 
-/sbin/busybox sh /sbin/ext/modules.sh
+. /res/customconfig/customconfig-helper
+read_defaults
+read_config
+
+mkdir /data/.siyah
+chmod 777 /data/.siyah
+[ ! -f /data/.siyah/default.profile ] && cp /res/customconfig/default.profile /data/.siyah
+[ ! -f /data/.siyah/battery.profile ] && cp /res/customconfig/battery.profile /data/.siyah
+[ ! -f /data/.siyah/performance.profile ] && cp /res/customconfig/performance.profile /data/.siyah
+
+if [ "$logger" == "on" ];then
+insmod /lib/modules/logger.ko
+fi
+
+# disable debugging on some modules
+if [ "$logger" == "off" ];then
+  rm -rf /dev/log
+  echo 0 > /sys/module/ump/parameters/ump_debug_level
+  echo 0 > /sys/module/mali/parameters/mali_debug_level
+  echo 0 > /sys/module/kernel/parameters/initcall_debug
+  echo 0 > /sys//module/lowmemorykiller/parameters/debug_level
+  echo 0 > /sys/module/wakelock/parameters/debug_mask
+  echo 0 > /sys/module/userwakelock/parameters/debug_mask
+  echo 0 > /sys/module/earlysuspend/parameters/debug_mask
+  echo 0 > /sys/module/alarm/parameters/debug_mask
+  echo 0 > /sys/module/alarm_dev/parameters/debug_mask
+  echo 0 > /sys/module/binder/parameters/debug_mask
+  echo 0 > /sys/module/xt_qtaguid/parameters/debug_mask
+fi
+
+# for ntfs automounting
+insmod /lib/modules/fuse.ko
 
 #/sbin/busybox sh /sbin/ext/busybox.sh
 
@@ -30,7 +61,10 @@ sleep 30
 
 ##### init scripts #####
 (
-sleep 12
+#apply default config, delay for 5 secs to make sure that everything is initialized
+sleep 5
+/res/uci.sh apply
+sleep 7
 /sbin/busybox sh /sbin/ext/run-init-scripts.sh
 )&
 
--- sbin/ext/tweaks.sh
+++ sbin/ext/tweaks.sh
@@ -2,31 +2,6 @@
 # thanks to hardcore and nexxx
 # some parameters are taken from http://forum.xda-developers.com/showthread.php?t=1292743 (highly recommended to read)
 
-. /res/customconfig/customconfig-helper
-read_defaults
-read_config
-
-#apply default config, delay for 5 secs to make sure that everything is initialized
-(
-sleep 5
-/res/uci.sh apply
-) &
-
-# disable debugging on some modules
-if [ "$logger" == "off" ];then
-  echo 0 > /sys/module/ump/parameters/ump_debug_level
-  echo 0 > /sys/module/mali/parameters/mali_debug_level
-  echo 0 > /sys/module/kernel/parameters/initcall_debug
-  echo 0 > /sys//module/lowmemorykiller/parameters/debug_level
-  echo 0 > /sys/module/wakelock/parameters/debug_mask
-  echo 0 > /sys/module/userwakelock/parameters/debug_mask
-  echo 0 > /sys/module/earlysuspend/parameters/debug_mask
-  echo 0 > /sys/module/alarm/parameters/debug_mask
-  echo 0 > /sys/module/alarm_dev/parameters/debug_mask
-  echo 0 > /sys/module/binder/parameters/debug_mask
-  echo 0 > /sys/module/xt_qtaguid/parameters/debug_mask
-fi
-
 #thanks to pikachu01@XDA
 /sbin/busybox sh /sbin/ext/thunderbolt.sh
 mount -o noatime,remount,rw,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /cache /cache;
