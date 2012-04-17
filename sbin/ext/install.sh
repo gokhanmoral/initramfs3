3b5e4092c9079a2435b66e2cc8d1fdfef31b3d7f
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-17 14:53:01 +0300

    fix ntfs automounting in readonly mode for usb sticks

--- sbin/ext/install.sh
+++ sbin/ext/install.sh
@@ -86,6 +86,8 @@ romtype=`cat /proc/sys/kernel/rom_feature_set`
 #fi
 
 echo "ntfs-3g..."
+mkdir /mnt/ntfs
+mount -t tmpfs tmpfs /mnt/ntfs
 if [ ! -s /system/xbin/ntfs-3g ];
 then
   if [ "$payload_extracted" == "0" ];then
